5a7f58c1ff98eaf50d8163b4ba55d6400ea25108
jodsol <jodsol94@gmail.com>
2023-10-03 13:46:48 +0900

    chore: opengl project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/조혜형/git/opengl
+# Build directory: C:/Users/조혜형/git/opengl/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
